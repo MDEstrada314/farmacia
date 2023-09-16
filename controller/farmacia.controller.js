d7f9f512a422123138eed3b50dd7558c7da124ea
MDEstrada314 <mdelprograma@gmail.com>
2023-09-16 07:42:13 -0500

    ejercicio32

--- controller/farmacia.controller.js
+++ controller/farmacia.controller.js
@@ -33,9 +33,8 @@ const ejercio1 = async (req,res)=>{
 const ejercio2 = async (req,res)=>{
     try {
         const colecion =  await getCollection('Medicamentos')
-        const result = await colecion.distinct('proveedor') 
+        const result = await colecion.distinct('proveedor')
         res.json(result)
-        
         cliente.close()
 
     } catch (error) {
@@ -325,25 +324,606 @@ const ejercio13 = async (req,res)=>{
         const colecion =  await getCollection('Compras')
         const fecha_busqueda = new Date('2023-01-01');
         colecion.find({"fechaCompra": {"$lt": fecha_busqueda}}).toArray()
-    
             .then(resultados => {
-                const fechaFinal = []
-                const fechas = resultados.map(documento => documento.fechaCompra);
-                for (let index = 0; index < fechas.length; index++) {
-                    const element = `fecha ${index} ---> ${fechas[index]}`;
-                    fechaFinal.push(element)
-    
-                    
-                }
-            res.json(fechaFinal);
+                res.json(resultados) 
+                })
 
-            })
         } catch (error) {
-        
             res.status(404).json("no se encontro datos")
-        
     }
 }
+
+const ejercio14 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+        const fecha_inicio = new Date('2023-05-01');
+        const fecha_final = new Date('2023-05-30');
+       
+        
+        const result = await colecion.find({
+            fechaVenta: { $gte: fecha_inicio, $lte: fecha_final }
+        }).toArray();
+
+        
+        res.json(result);
+
+        } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}
+
+// Obtener el medicamento menos vendido en 2023.
+const ejercio15 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+        const result = await colecion.aggregate([
+            {
+                $unwind: "$medicamentosVendidos"
+            },
+            {
+                $match: {
+                    "fechaVenta": {
+                        $gte: new Date("2023"),
+                        $lte: new Date("2024")
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$medicamentosVendidos.nombreMedicamento",
+                    totalVendido: { $sum: "$medicamentosVendidos.cantidadVendida" }
+                }
+            },
+            {
+                $sort: { totalVendido: 1 }
+            },
+            {
+                $limit: 1
+            }
+        ]).toArray();
+        res.json(result)
+        cliente.close()
+    } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}   
+
+const ejercio16 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Compras')
+        const result = await colecion.aggregate([
+            {
+                $unwind: "$medicamentosComprados"
+            },
+            {
+                $group:{
+                    _id:"$proveedor.nombre",
+                    gananciasPesos: { $sum:"$medicamentosComprados.precioCompra"}
+
+                }
+            },
+            {
+                $sort: { _id: 1 } 
+              }
+
+        ]).toArray()
+
+        
+        res.json(result);
+
+        } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}
+
+
+
+
+// Promedio de medicamentos comprados por venta.
+const ejercio17 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+        const result = await colecion.aggregate([
+            {
+                $unwind: "$medicamentosVendidos"
+            },
+            {
+                $group: {
+                    _id: "$_id",
+                    totalMedicamentos: { $sum: "$medicamentosVendidos.cantidadVendida" },
+                    count: { $sum: 1 }
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    promedioMedicamentosPorVenta: { $avg: "$totalMedicamentos" }
+                }
+            },
+            {
+                $project:{
+                    _id: 0
+                }
+            }
+        ]).toArray();
+
+        res.json(result[0]); 
+        cliente.close()
+    } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}  
+
+// Cantidad de ventas realizadas por cada empleado en 2023.
+const ejercio18 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+                $match: {
+                    fechaVenta: {
+                        $gte: new Date("2023"),
+                        $lt: new Date("2024")
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$empleado.nombre",
+                    cantidadVentas: { $sum: '$medicamentosVendidos.cantidadVendida' }
+                }
+            }
+        ]).toArray();
+
+        if (result.length > 0) {
+            res.json(result);
+        } else {
+            res.status(404).json("No se encontraron datos");
+        }
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+const ejercio19 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Medicamentos')
+        const date_init = new Date('2024-01-01')
+        const date_fin = new Date('2024-12-31')
+        const result = await colecion.find({fechaExpiracion: { $gte:date_init , $lte:date_fin}}).toArray()
+        res.json(result)
+
+      
+    } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}  
+
+
+const ejercio20 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+
+        const result = await colecion.aggregate([
+          {
+            $unwind:'$medicamentosVendidos'
+          },
+          {
+            $group:{
+                _id:"$empleado.nombre",
+                cantidadVendida:{ $sum:"$medicamentosVendidos.cantidadVendida"}
+            }
+          }
+
+        ]).toArray()
+        let result2 = []
+        result.map((e)=>{
+            if(e.cantidadVendida > 5){
+                result2.push(`nombre:${e._id} -------- Total ventas :${e.cantidadVendida}`)
+                
+            }
+        })
+        res.json(result2)
+
+
+
+
+      
+    } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}  
+
+
+const ejercio21 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Ventas')
+        const medicamentos = await colección.aggregate([
+            {
+                $lookup:{
+                    from:"Compras",
+                    localField: "medicamentosVendidos.nombreMedicamento",
+                    foreignField: "medicamentosComprados.nombreMedicamento",
+                    as: "vendidos"
+                }
+            },
+            {
+                $match: {
+                  "vendidos": {$size:0}
+                }
+            }
+        
+        
+        ]).toArray()
+        res.json(medicamentos)
+
+          
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+
+
+const ejercio22 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+                $match: {
+                    fechaVenta: {
+                        $gte: new Date("2023"),
+                        $lt: new Date("2024")
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: "$paciente.nombre",
+                    cantidadVendida: { $sum: '$medicamentosVendidos.precio' }
+                }
+               
+            },
+            {
+                $sort:{precio:-1}
+            },
+            {
+                $limit:1
+            } 
+        ]).toArray();
+
+            res.json(result);
+      
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+
+const ejercio23 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Empleados');
+        const result = await coleccion.aggregate([
+            {
+                $lookup: {
+                    from: 'Ventas',
+                    localField: 'nombre',
+                    foreignField: 'empleado.nombre',
+                    as: 'ventas'
+                }
+            },
+            {
+                $match: {
+                    ventas: { $size: 0 },
+                }
+            }
+        ]).toArray();
+        res.json(result)
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+const ejercio24 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Compras');
+        const result = await coleccion.aggregate([
+            {
+                $match: {
+                  fechaCompra: {
+                    $gte: new Date("2023"),
+                    $lt: new Date("2024")
+                  }
+                }
+              },
+              {
+                $unwind: "$medicamentosComprados"
+              },
+              {
+                $group: {
+                  _id: "$proveedor.nombre",
+                  cantidadSuministrada: { $sum: "$medicamentosComprados.cantidadComprada" }
+                }
+              },
+              {
+                $sort: {
+                  cantidadSuministrada: -1
+                }
+              },
+              {
+                $limit: 1
+              }
+        ]).toArray();
+        res.json(result)
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+
+
+const ejercio25 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+                $match: {
+                    fechaVenta: {
+                        $gte: new Date("2023"),
+                        $lt: new Date("2024")
+                    },
+                    'medicamentosVendidos.nombreMedicamento':'Paracetamol'
+
+                }
+            },
+            {
+                $group: {
+                    _id: "$paciente.nombre",
+                    registros: { $push: "$$ROOT" }
+                }
+               
+            }
+
+        ]).toArray();
+
+            res.json(result);
+      
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+
+const ejercio26 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+              $group: {
+                _id: { $month: "$fechaVenta" },
+                totalVentas: { $sum: '$medicamentosVendidos.cantidadVendida' },
+              },
+            },
+            {
+              $sort: { _id: 1 },
+            },
+          ])
+          .toArray();
+        res.json(result);
+      
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+   
+    }
+
+}
+
+const ejercio27 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+
+        const result = await colecion.aggregate([
+          {
+            $unwind:'$medicamentosVendidos'
+          },
+          {
+            $group:{
+                _id:"$empleado.nombre",
+                cantidadVendida:{ $sum:"$medicamentosVendidos.cantidadVendida"}
+            }
+          }
+
+        ]).toArray()
+        let result2 = []
+        result.map((e)=>{
+            if(e.cantidadVendida < 5){
+                result2.push(`nombre:${e._id} -------- Total ventas :${e.cantidadVendida}`)
+                
+            }
+        })
+        res.json(result2)
+
+
+      
+    } catch (error) {
+            res.status(404).json("no se encontro datos")
+    }
+}  
+
+
+
+const ejercio28 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Compras');
+        const result = await coleccion.aggregate([
+            {
+                $match: {
+                    fechaCompra: {
+                        $gte: new Date("2023"),
+                        $lt: new Date("2024")
+                    },
+                }
+            },
+            {
+                $group: {
+                    _id: "$proveedor.nombre"
+                }
+            }
+
+        ]).toArray();
+        const total = result.length
+            res.json({
+                total,result});
+      
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+
+
+const ejercio29 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Medicamentos');
+        const result = await coleccion.aggregate([
+            {
+                $match: {
+                    stock: {
+                        $lte: 50
+                        
+                    },
+                }
+            },
+            {
+                $group: {
+                    _id: "$proveedor.nombre",
+                    registros: { $push: "$$ROOT" }
+                }
+            }
+
+        ]).toArray();
+        const total = result.length
+            res.json({
+                total,result});
+      
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+    }
+} 
+
+const ejercio30 = async(req,res)=>{
+   try {
+    const coleccion = await getCollection('Pacientes')
+    const result = await coleccion.aggregate([
+
+        {
+            $lookup: {
+                from: 'Ventas',
+                localField: 'nombre',
+                foreignField: 'paciente.nombre',
+                as: 'ventas'
+            }
+        },
+        {
+            $match: {
+                ventas: { $size: 0 },
+            }
+        }
+    ]).toArray();
+    res.json(result)
+
+   } catch (error) {
+    res.status(500).json("Error interno del servidor");
+   }
+}
+
+
+const ejercio31 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+              $group: {
+                  _id: { $month: "$fechaVenta" },
+                  medicamento:{ $push: '$medicamentosVendidos.nombreMedicamento'},
+                  totalVentas: { $sum: '$medicamentosVendidos.cantidadVendida' },
+              },
+            },
+            {
+              $sort: { _id: 1 },
+            },
+          ])
+          .toArray();
+        res.json(result);
+      
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+   
+    }
+
+}
+
+
+const ejercio32 = async (req, res) => {
+    try {
+        const coleccion = await getCollection('Ventas');
+        const result = await coleccion.aggregate([
+            {
+                $unwind:'$medicamentosVendidos'
+            },
+            {
+                $match: {
+                    fechaVenta: {
+                        $gte: new Date("2023"),
+                        $lt: new Date("2024")
+                    },
+                }
+            },
+          
+            {
+              $group: {
+                  _id:"$empleado.nombre",
+                  medicamento:{ $push: '$medicamentosVendidos.nombreMedicamento'},
+                  totalVentas: { $sum: '$medicamentosVendidos.cantidadVendida' },
+              },
+            },
+            {
+              $sort: { totalVentas: -1 },
+            },
+            {
+                $limit:1
+            }
+          ])
+          .toArray();
+        res.json(result);
+      
+
+    } catch (error) {
+        res.status(500).json("Error interno del servidor");
+   
+    }
+
+}
+
 
 
 
@@ -351,5 +931,7 @@ const ejercio13 = async (req,res)=>{
 
 module.exports = {
     ejercio1,ejercio2,ejercio3,ejercio4,ejercio5,ejercio6,ejercio7,ejercio8,ejercio9,
-    ejercio10,ejercio11,ejercio12,ejercio13
+    ejercio10,ejercio11,ejercio12,ejercio13,ejercio14,ejercio15,ejercio16,ejercio17,ejercio18,ejercio19,
+    ejercio20,ejercio21,ejercio22,ejercio23,ejercio24,ejercio25,ejercio26,ejercio27,ejercio28,ejercio29,ejercio30,ejercio31,
+    ejercio32
 }
--- router/router.js
+++ router/router.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
-const { ejercio1,ejercio2,ejercio3, ejercio4, ejercio5, ejercio6, ejercio7, ejercio8, ejercio9, ejercio10, ejercio11, ejercio12, ejercio13 } = require('../controller/farmacia.controller')
+const { ejercio1,ejercio2,ejercio3, ejercio4, ejercio5, ejercio6, ejercio7, ejercio8, ejercio9,
+     ejercio10, ejercio11, ejercio12, ejercio13, ejercio14, ejercio15, ejercio16, ejercio17, 
+     ejercio18, ejercio19, ejercio20, ejercio21, ejercio22, ejercio23, ejercio24, ejercio25, ejercio26, ejercio27, ejercio28, ejercio29, ejercio30, ejercio31, ejercio32 } = require('../controller/farmacia.controller')
 
 
 
@@ -13,6 +15,11 @@ router.get('/hola', async(req,res)=>{
 
 
 //Rutas
+/* const numero = 38
+for (let index = 1; index < numero; index++) {
+    const ejercio = "ejercio";
+    router.get(`/ejercio${index}`, ejercio,`${index}`)
+} */
 router.get('/ejercio1', ejercio1)
 router.get('/ejercio2', ejercio2)
 router.get('/ejercio3', ejercio3)
@@ -26,6 +33,28 @@ router.get('/ejercio10', ejercio10)
 router.get('/ejercio11', ejercio11)
 router.get('/ejercio12', ejercio12)
 router.get('/ejercio13', ejercio13)
+router.get('/ejercio14', ejercio14)
+router.get('/ejercio15', ejercio15)
+router.get('/ejercio16', ejercio16)
+router.get('/ejercio17', ejercio17)
+router.get('/ejercio18', ejercio18)
+router.get('/ejercio19', ejercio19)
+router.get('/ejercio20', ejercio20)
+router.get('/ejercio21', ejercio21)
+router.get('/ejercio22', ejercio22)
+router.get('/ejercio22', ejercio22)
+router.get('/ejercio23', ejercio23)
+router.get('/ejercio24', ejercio24)
+router.get('/ejercio25', ejercio25)
+router.get('/ejercio26', ejercio26)
+router.get('/ejercio27', ejercio27)
+router.get('/ejercio28', ejercio28)
+router.get('/ejercio29', ejercio29)
+router.get('/ejercio30', ejercio30)
+router.get('/ejercio31', ejercio31)
+router.get('/ejercio32', ejercio32)
+
+
 
 
 
