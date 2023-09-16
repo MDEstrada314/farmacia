409a17ec4241f875b58e137aa473c2a50729e508
MDEstrada314 <mdelprograma@gmail.com>
2023-09-15 23:15:45 -0500

    CORRECION SIMULACRO FILTRO

--- controller/farmacia.controller.js
+++ controller/farmacia.controller.js
@@ -1,3 +1,4 @@
+const { json } = require('express/lib/response')
 const { MongoClient } = require('mongodb')
 require ('dotenv').config()
 
@@ -133,7 +134,10 @@ const ejercio6 = async (req,res)=>{
     });
 }
 
-const ejercio7 = async(req,res)=>{
+
+
+
+/* const ejercio7 = async(req,res)=>{
     try {
         const colección = await getCollection('Medicamentos')
         const nombres = await colección.aggregate([
@@ -152,11 +156,199 @@ const ejercio7 = async(req,res)=>{
 
 
 }
+ */
+
+const ejercio7 = async(req,res)=>{
+    try {
+        const colección = await getCollection('Compras')
+        const pipeline = await colección.aggregate ([
+            {
+              $unwind: "$medicamentosComprados" // Desagregar el array de medicamentos
+            },
+            {
+              $group: {
+                _id: '$proveedor.nombre',
+                totalCantidadComprada: { $sum: "$medicamentosComprados.cantidadComprada" } // Sumar cantidadComprada por nombreMedicamento
+              }
+            },
+            {
+              $sort: { _id: 1 } // Ordenar por nombreMedicamento en orden ascendente (1) o descendente (-1)
+            }
+          ]).toArray();
+      
+         
+      
+          res.json(pipeline);
+        } catch (error) {
+          console.error('Error al calcular la cantidad comprada por medicamento:', error);
+        } 
+}
+
+
+/* const ejercio8 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Ventas')
+        const valor = await colección.aggregate([
+            {
+            $unwind: "$medicamentosVendidos"
+            },
+            {
+                $group: {
+                    _id:null,
+                    precioTotal: { $sum: { $multiply: ["$medicamentosVendidos.cantidadVendida", "$medicamentosVendidos.precio"] } } // Calcular el precio total
+                }
+            }
+    ]).toArray()
+    res.json(valor)
+        
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+ */
+
+
+const ejercio8 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Ventas')
+        const valor = await colección.find().toArray()
+        let valorTotal = 0
+        valor.forEach(element => {
+            element.medicamentosVendidos.forEach(doc =>{
+                valorTotal += doc.cantidadVendida * doc.precio
+            })
+
+            
+        });
+        res.json({
+            totalVentas :`$ ${valorTotal}`
+        })
+
+          
+
+        
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+
+
+const ejercio9 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Medicamentos')
+        const medicamentos = await colección.aggregate([
+            {
+                $lookup:{
+                    from:"Ventas",
+                    localField: "nombre",
+                    foreignField: "medicamentosVendidos.nombreMedicamento",
+                    as: "vendidos"
+                }
+            },
+            {
+                $match: {
+                  "vendidos": {$size:0}
+                }
+            }
+        
+        
+        ]).toArray()
+        res.json(medicamentos)
+
+          
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+
+const ejercio10 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Medicamentos')
+        const medicamentos = await colección.find().sort({precio:-1}).limit(1).toArray()
+        res.json(medicamentos)
+
+          
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+
+const ejercio11 = async (req,res) =>{
+    try {
+        const colección = await getCollection('Medicamentos')
+        const medicamentos = await colección.find().toArray();
+        let totales = [0,0,0]
+        medicamentos.map((e)=>{
+            if(e.proveedor.nombre == 'ProveedorA'){
+                totales[0]++;
+            }
+            if(e.proveedor.nombre == 'ProveedorB'){
+                totales[1]++
+            }
+            else if(e.proveedor.nombre == 'ProveedorC'){totales[2]++}
+    })
+        const mediProvee = [
+            {proveedorA: `${totales[0]} medicamnetos`},
+            {proveedorB: `${totales[1]} medicamnetos`},
+            {proveedorC: `${totales[2]} medicamnetos`},
+        ]
+
+        res.json({
+            CantidadMedicamentos: mediProvee
+        })  
+
+          
+    } catch (error) {
+        console.error('Error al calcular la cantidad comprada por medicamento:', error);
+    }
+}
+
+const ejercio12 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Ventas')
+        const result = await colecion.find({'medicamentosVendidos.nombreMedicamento':'Paracetamol'})
+        .project({ "_id": 0, "paciente.nombre": 1, "medicamentosVendidos.nombreMedicamento": 1 }).toArray()
+        res.json(result)
+        cliente.close()
+
+    } catch (error) {
+        
+            res.status(404).json("no se encontro datos")
+        
+    }
+}
+
+
+const ejercio13 = async (req,res)=>{
+    try {
+        const colecion =  await getCollection('Compras')
+        const fecha_busqueda = new Date('2023-01-01');
+        colecion.find({"fechaCompra": {"$lt": fecha_busqueda}}).toArray()
+    
+            .then(resultados => {
+                const fechaFinal = []
+                const fechas = resultados.map(documento => documento.fechaCompra);
+                for (let index = 0; index < fechas.length; index++) {
+                    const element = `fecha ${index} ---> ${fechas[index]}`;
+                    fechaFinal.push(element)
+    
+                    
+                }
+            res.json(fechaFinal);
+
+            })
+        } catch (error) {
+        
+            res.status(404).json("no se encontro datos")
+        
+    }
+}
 
 
 
 
 
 module.exports = {
-    ejercio1,ejercio2,ejercio3,ejercio4,ejercio5,ejercio6,ejercio7
+    ejercio1,ejercio2,ejercio3,ejercio4,ejercio5,ejercio6,ejercio7,ejercio8,ejercio9,
+    ejercio10,ejercio11,ejercio12,ejercio13
 }
--- router/router.js
+++ router/router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { ejercio1,ejercio2,ejercio3, ejercio4, ejercio5, ejercio6, ejercio7 } = require('../controller/farmacia.controller')
+const { ejercio1,ejercio2,ejercio3, ejercio4, ejercio5, ejercio6, ejercio7, ejercio8, ejercio9, ejercio10, ejercio11, ejercio12, ejercio13 } = require('../controller/farmacia.controller')
 
 
 
@@ -12,7 +12,7 @@ router.get('/hola', async(req,res)=>{
 })  
 
 
-//ejercion 1
+//Rutas
 router.get('/ejercio1', ejercio1)
 router.get('/ejercio2', ejercio2)
 router.get('/ejercio3', ejercio3)
@@ -20,6 +20,13 @@ router.get('/ejercio4', ejercio4)
 router.get('/ejercio5', ejercio5)
 router.get('/ejercio6', ejercio6)
 router.get('/ejercio7', ejercio7)
+router.get('/ejercio8', ejercio8)
+router.get('/ejercio9', ejercio9)
+router.get('/ejercio10', ejercio10)
+router.get('/ejercio11', ejercio11)
+router.get('/ejercio12', ejercio12)
+router.get('/ejercio13', ejercio13)
+
 
 
 
